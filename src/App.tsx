677ee4c83764b33390da065ec171d26b1886bd17
Agam Julio <agam.julio@365.ono.ac.il>
2025-11-27 16:58:59 +0200

    Added react project (by Agam)

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <h1>Welcome to my React App</h1>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
